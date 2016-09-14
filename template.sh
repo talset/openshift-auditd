ae5490fc06a008357898fb5a52c83155e569b7bf
root <root@master.local.bm.chmouel.com>
2016-09-14 12:47:51 -0400

    update yml

--- template.sh
+++ template.sh
@@ -2,9 +2,9 @@
 
 #PROXY='-p PROXY=http://10.205.187.161:8888'
 
-oc delete all --all; oc delete template --all
+oc delete all --all; oc delete template --all; oc delete ds --all
 
-oc create -f openshift-auditd-template.yml
+oc create -f dump.yaml
 oc new-app --template=openshift-auditd
-sleep 3
-oc start-build openshift-auditd
+#sleep 3
+#oc start-build openshift-auditd
